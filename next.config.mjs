a6439ec567fa66d9734730c0fde1b76b8ed854ca
dakshpandey177@gmail.com <dakshpandey177@gmail.com>
2025-06-18 14:59:34 +0530

    vercel

--- next.config.mjs
+++ next.config.mjs
@@ -12,13 +12,4 @@ const nextConfig = {
 }
 
 export default nextConfig
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  }
-}
 
-module.exports = nextConfig
