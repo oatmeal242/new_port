2088b5e20c30f1754494c19720c35e84299bf17d
dakshpandey177@gmail.com <dakshpandey177@gmail.com>
2025-06-18 14:58:12 +0530

    github pages

--- next.config.mjs
+++ next.config.mjs
@@ -12,3 +12,13 @@ const nextConfig = {
 }
 
 export default nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
+}
+
+module.exports = nextConfig
